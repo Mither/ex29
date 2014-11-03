6621988db5fe0f575e2355c8752c78dd9387e880
Mithermayer Menabo Neto <mithermayerneto@hotmail.com>
2014-11-03 11:20:37 -0300

    mensagem

--- ex29.c
+++ ex29.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	printf("1- coca R$3,50 \n 2- ice R$5,80 \n 3- guarana R$2,10 \n 4- suco R$1,90 \n 5- café R$0,70");
+	printf(" 1- coca R$3,50 \n 2- ice R$5,80 \n 3- guarana R$2,10 \n 4- suco R$1,90 \n 5- café R$0,70");
 	return 0;
 }
