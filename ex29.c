a23f01b60e263be5f889b6a37aa75527362bd941
Mithermayer Menabo Neto <mithermayerneto@hotmail.com>
2014-11-03 11:41:35 -0300

    mensagem completa

--- ex29.c
+++ ex29.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-	printf(" 1- coca R$3,50 \n 2- ice R$5,80 \n 3- guarana R$2,10 \n 4- suco R$1,90 \n 5- café R$0,70\n");
+	int p;
+	float v;
+
+	printf(" 1- coca R$3,50 \n 2- ice R$5,80 \n 3- guarana R$2,10 \n 4- suco R$1,90 \n 5- café R$0,70\n\n\n Qual produto deseja?\n");
+	scanf("%d", &p);
+	printf(" Qual o valor inserido?\n");
+	scanf("%f", &v);
 	return 0;
 }
