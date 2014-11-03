3d99304deee52fd82b5b4c5322e37f9a6406b012
Mithermayer Menabo Neto <mithermayerneto@hotmail.com>
2014-11-03 12:05:32 -0300

    comentada

--- ex29.c
+++ ex29.c
@@ -1,13 +1,20 @@
+/* Este programa calcula o troco e de que forma ele deve ser dado.
+ * Autor: Mithermayer Menabó Neto
+ * Data: 03/11/2014
+ */
+
 #include <stdio.h>
 
 int main(void)
 {
-	int p;
-	float v;
+	int p; /*indica produto*/
+	float v; /*indica valor do produto*/
 
 	printf(" 1- coca R$3,50 \n 2- ice R$5,80 \n 3- guarana R$2,10 \n 4- suco R$1,90 \n 5- café R$0,70\n\n\n Qual produto deseja?\n");
 	scanf("%d", &p);
 	printf(" Qual o valor inserido?\n");
 	scanf("%f", &v);
+
 	return 0;
 }
+
